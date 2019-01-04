08ab334a37b83d6dbd8bf63a1be281a7e5272885
Phaseka Malatji <phaseka@bbd.co.za>
2019-01-04 20:37:56 +0200

    simple dynamics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DynamoFormo';
+  user = {
+    name: 'Phaseka',
+    age: '28',
+    bitches: 1
+  }
+
+  daForm: FormGroup;
+
+  ngOnInit() {
+    this.daForm = new FormGroup({
+      [this.fieldName]: new FormControl(this.fieldVal),
+
+    });
+  }
 }
